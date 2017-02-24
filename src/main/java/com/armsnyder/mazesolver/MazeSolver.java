398ba3a8a30585fe36aaa9f2f82ebfaa93ff2f34
Adam Snyder <armsnyder@gmail.com>
2017-02-24 12:37:29 -0800

    stub main class

---
+++ src/main/java/com/armsnyder/mazesolver/MazeSolver.java
@@ -0,0 +1,10 @@
+package com.armsnyder.mazesolver;
+
+/**
+ * Created by asnyder on 2/24/17.
+ */
+public class MazeSolver {
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException("empty main method");
+    }
+}
